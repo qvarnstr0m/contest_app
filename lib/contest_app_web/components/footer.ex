23c126d55ee52e096be05d4ea81102dcb9926c05
qvarnstr0m <mqvarnstrom80@gmail.com>
2025-03-08 23:08:54 +0100

    Link update on footer

--- lib/contest_app_web/components/footer.ex
+++ lib/contest_app_web/components/footer.ex
@@ -4,7 +4,12 @@ defmodule Footer do
   def footer(assigns) do
     ~H"""
     <div class="flex flex-row justify-end py-4 md:px-20">
-      <p>Phoenix Framework v{Application.spec(:phoenix, :vsn)}</p>
+      <p>
+        <a href="https://www.phoenixframework.org/" target="_blank" rel="noopener noreferrer">
+          Phoenix Framework
+        </a>
+        v{Application.spec(:phoenix, :vsn)}
+      </p>
       <div>
         <.link href="/readme" class="ms-2">ReadMe</.link>
       </div>
